8ca9993f4f0ef2e95b6e398139fe38fbd9c299d9
lucas-olvra <lucas2000544@gmail.com>
2024-05-24 16:07:57 -0300

    :feat: build e uso da biblioteca faker do Java

--- src/test/java/br/com/mentoria/automacao/web/SwitchTo.java
+++ src/test/java/br/com/mentoria/automacao/web/SwitchTo.java
@@ -1,14 +1,32 @@
 package br.com.mentoria.automacao.web;
 
 import br.com.mentoria.automacao.web.commons.Dsl;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class SwitchTo extends Dsl {
 
-    @Test
-    public void actionBySwitchTo(){
+    @Before
+    public void beforeTest(){
         super.accessSite("https://demo.automationtesting.in/Register.html");
         super.actionByXpath("//a[@href='SwitchTo.html' and text()='SwitchTo']");
+
+    }
+
+    @Test
+    public void actionBySwitchTo(){
         super.clickByXpath("//a[@href='Frames.html' and text()='Frames']");
+    }
+
+    @Test
+    public void iFrameTest(){
+        super.clickById("cbb");
+
+    }
+
+    @After
+    public void afterTest(){
+        driver.quit();
     }
 }
