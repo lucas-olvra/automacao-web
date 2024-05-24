cfa78c48189da034de413dae0a014315db158397
lucas-olvra <lucas2000544@gmail.com>
2024-05-24 16:55:24 -0300

    interagindo com um alert e aceitando

--- src/test/java/br/com/mentoria/automacao/web/SwitchTo.java
+++ src/test/java/br/com/mentoria/automacao/web/SwitchTo.java
@@ -4,29 +4,39 @@ import br.com.mentoria.automacao.web.commons.Dsl;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 public class SwitchTo extends Dsl {
 
     @Before
-    public void beforeTest(){
+    public void beforeTest() {
         super.accessSite("https://demo.automationtesting.in/Register.html");
         super.actionByXpath("//a[@href='SwitchTo.html' and text()='SwitchTo']");
 
     }
 
     @Test
-    public void actionBySwitchTo(){
+    public void actionBySwitchTo() {
         super.clickByXpath("//a[@href='Frames.html' and text()='Frames']");
     }
 
     @Test
-    public void iFrameTest(){
+    public void iFrameTest() {
         super.clickById("cbb");
 
     }
 
+    @Test
+    public void interactionAlertOk() {
+        super.clickByXpath("//a[@href='Alerts.html']");
+        super.clickByXpath("//button[@onclick='alertbox()']");
+        super.driver.switchTo().alert().accept();
+
+    }
+
     @After
-    public void afterTest(){
+    public void afterTest() {
         driver.quit();
     }
 }
